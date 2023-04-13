125484ad5bfdffa57bbf8bfad8562d6889678332
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-04-13 16:57:39 +0530

    Time: 81 ms (37.30%), Space: 14.1 MB (25.77%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    int numberOfSubstrings(string s) {
+        
+    unordered_map<char,int> mp;  
+    int count=0;
+    int i=0; int j=0;
+    int end=s.size()-1;
+
+    while(j<s.size()){
+        //calculations while travsering j 
+        mp[s[j]]++;
+
+        //when window size is less than reqd
+        if(mp.size()<3){
+            //we need to traverse j 
+            j++;
+        }
+        else if(mp.size()==3){
+            //once it reached, we save the count and shrink the window size from left 
+            // till the time, we have 3 distinct chars, we can go on calculating count and increase i but not increasing j
+            //eg input 'aaacb' 
+            while(mp.size()==3){
+                count+=1+end-j;
+                mp[s[i]]--;
+            if(mp[s[i]]==0){
+                mp.erase(s[i]);
+            }
+            i++;
+            }
+            j++;
+        }
+    } return count;
+    }
+};
