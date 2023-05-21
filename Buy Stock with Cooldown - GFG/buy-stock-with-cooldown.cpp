5142c66486d1685fc8acda467c977fe09fe60353
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-05-21 09:31:30 +0530

    Added solution - LeetHub

---
+++ Buy Stock with Cooldown - GFG/buy-stock-with-cooldown.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long maximumProfit(vector<long long>&Arr, int n) {
+    //   int n = Arr.size();
+    vector<vector<long long>> dp(n+2,vector<long long>(2,0));
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit;
+            
+            if(buy==0){// We can buy the stock
+                profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+dp[ind+1][1], Arr[ind] + dp[ind+2][0]);
+            }
+            
+            dp[ind][buy] = profit;
+        }
+    }
+    
+    return dp[0][0];
+}
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
