da922147373f66a1222b55b9df139b3ce2810bb4
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-06-13 18:39:45 +0530

    Time: 10 ms (48.07%), Space: 9.3 MB (8.90%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.cpp
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    string concatinate(int a,int b){
-        return to_string(a)+to_string(b);
-    }
     long long findTheArrayConcVal(vector<int>& nums) {
-        // long long int mod=1e19+7;
-        long long int ans=0;
-        long long int n =nums.size(); 
-        long long int i=0, j=n-1;
-        if(n%2!=0) ans+=nums[(n+1)/2-1];
-        while(i<j){
-            string s = concatinate(nums[i],nums[j]);
-            cout<<s<<endl;
-            (ans+=stoi(s));
+        long long ans = 0;
+        int n = nums.size(); 
+        int i = 0, j = n - 1;
+
+        if (n % 2 != 0)
+            ans += nums[n / 2];
+
+        while (i < j) {
+            int concatenated = nums[i] * pow(10, getDigits(nums[j])) + nums[j];
+            ans += concatenated;
             i++;
             j--;
         }
+
         return ans;
+    }
+
+private:
+    int getDigits(int num) {
+        int digits = 0;
+        while (num > 0) {
+            num /= 10;
+            digits++;
+        }
+        return digits;
     }
 };
