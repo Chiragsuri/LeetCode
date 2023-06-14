14e389a7cabf4de03f50d6557d8d7eb0f8c8c309
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-06-14 14:44:59 +0530

    Time: 7 ms (62.24%), Space: 9.9 MB (79.83%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,20 +1,42 @@
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> mp;
-        for (auto it : nums1) {
-            mp[it]++;
+        if (nums1.empty() || nums2.empty()) {
+            return {};
         }
+
+        sort(nums1.begin(), nums1.end());
+
         vector<int> ans;
-        for (auto it : nums2) {
-            if (mp.find(it) != mp.end() && mp[it] > 0) {
-                ans.push_back(it);
-                mp[it] = 0;
+        for (int num : nums2) {
+            if (binarySearch(nums1, num) && find(ans.begin(), ans.end(), num) == ans.end()) {
+                ans.push_back(num);
             }
         }
+
         return ans;
+    }
+
+private:
+    bool binarySearch(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
     }
 };
