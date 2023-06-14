edfa60310da689700489bc3c44bae8102a995cbe
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-06-14 15:42:24 +0530

    Time: 9 ms (82.06%), Space: 13 MB (94.61%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        sort(arr2.begin(), arr2.end());  // Sort arr2 for binary search
+
         int count = 0;
-        for(int i = 0; i < arr1.size(); i++) {
-            bool valid = true;
-            for(int j = 0; j < arr2.size(); j++) {
-                if(abs(arr1[i] - arr2[j]) <= d) {
-                    valid = false;
-                    break;
-                }
-            }
-            if(valid) {
+        for (int num : arr1) {
+            if (binarySearch(arr2, num, d)) {
                 count++;
             }
         }
+
         return count;
+    }
+
+private:
+    bool binarySearch(const vector<int>& arr, int target, int d) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (abs(arr[mid] - target) <= d) {
+                return false;  // Difference is within d, not a valid element
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return true;  // Difference is greater than d, valid element
     }
 };
