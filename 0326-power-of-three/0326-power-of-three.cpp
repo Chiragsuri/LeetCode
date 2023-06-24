f7a275db841451f568bb4bd8196df4b1f165eacd
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-06-24 19:52:14 +0530

    Time: 44 ms (5.68%), Space: 5.8 MB (80.99%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0) return false;
-        while(n%3==0)
-            n=n/3;
-        
-        return n==1;
+        if (n <= 0)
+            return false;
+        int maxPowerOfThree = pow(3, (int)(log10(INT_MAX) / log10(3))); // Maximum power of three that fits in an integer
+        return maxPowerOfThree % n == 0;
     }
 };
