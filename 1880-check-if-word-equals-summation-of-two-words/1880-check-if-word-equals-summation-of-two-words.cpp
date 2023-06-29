a2e8787244a0f6d3d11aa4f34ba3af626f590ba6
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-06-29 16:06:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (42.47%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool isSumEqual(string firstWord, string secondWord, string targetWord) {
-        int first=0,second=0,target=0;
-        for(int i=0;i<firstWord.size();i++)
-            first=first*10 + (firstWord[i]-'a');
+        return convertWordsToNum(firstWord) + convertWordsToNum(secondWord) == convertWordsToNum(targetWord);
         
-        for(int i=0;i<secondWord.size();i++)
-            second=second*10 +(secondWord[i]-'a');
-        
-        for(int i=0;i<targetWord.size();i++)
-            target=target*10 +(targetWord[i]-'a');
-        
-        
-        return first+second == target;
+    }
+   long convertWordsToNum(string str) {
+        long ans = 0;
+        for(auto i: str) {
+            ans = ans*10 + (i - 'a');
+        }
+        return ans;
     }
 };
