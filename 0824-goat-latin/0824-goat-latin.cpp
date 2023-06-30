44cc6f3dfcd94d9dd96e1fd82c55b47172cbda7b
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-06-30 15:38:10 +0530

    Time: 3 ms (46.25%), Space: 6.5 MB (49.19%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.cpp
+++ 0824-goat-latin/0824-goat-latin.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    string ans;
-    int i=1;
-    void isGoat(string temp){
-        char x = temp[0];
-        if(x=='a' || x=='A' || x=='e' || x=='E' || x=='i' || x=='I' || x=='o' || x=='O' || x=='u' || x=='U' ){
-            ans+=temp + "ma";
-        }
-        else{
-            ans+=temp.erase(0,1) + x + "ma";
+    string toGoatLatin(string sentence) {
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        stringstream ss(sentence);
+        string word, ans;
+        int i = 1;
+        
+        while (ss >> word) {
+            char firstChar = word[0];
+            
+            if (vowels.count(firstChar) > 0) {
+                ans += word;
+            } else {
+                ans += word.substr(1) + firstChar;
+            }
+            
+            ans += "ma" + string(i, 'a') + " ";
+            i++;
         }
         
-        ans+=string(i, 'a') + " ";
-        i++;
-    }
-    
-    string toGoatLatin(string sentence) {
-
-        string temp;
-        for(auto c:sentence){
-            if(!isspace(c))
-            {
-                temp+=c;
-            }
-            else{
-                
-                isGoat(temp);
-                temp="";
-            }
-                
-        }
-        isGoat(temp);
         ans.pop_back();
         return ans;
     }
