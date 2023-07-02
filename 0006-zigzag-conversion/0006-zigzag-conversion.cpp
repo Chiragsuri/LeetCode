e7a3b586c599cf3dcef7bc164872a13193bb4c47
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-02 15:04:15 +0530

    Time: 8 ms (80.15%), Space: 8.8 MB (71.32%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    string convert(std::string s, int numRows) {
+    string convert(string s, int numRows) {
+        // If numRows is less than or equal to 1 or the length of s is less than or equal to numRows,
+        // return the input string s as no conversion is required
         if (numRows <= 1 || s.length() <= numRows) {
             return s;
         }
+
         int len = s.length();
-        vector<int> vis(len, 0);
-        string ans = s.substr(0, 1);
-        vis[0] = 1;
+        vector<int> vis(len, 0); // Keeps track of visited indices in s
+        string ans = s.substr(0, 1); // Initialize the answer string with the first character of s
+        vis[0] = 1; // Mark the first character as visited
         int diagonal = numRows - 2;
         int i = numRows + diagonal, j = 0;
 
         while (ans.length() != len) {
+            // Check if i is within the valid range and not visited before
             if (i >= 0 && i < len && !vis[i]) {
-                ans += s[i];
-                vis[i] = 1;
+                ans += s[i]; // Add the character at index i to the answer string
+                vis[i] = 1; // Mark the character as visited
             }
-            i += numRows + diagonal - 2 * j;
+            i += numRows + diagonal - 2 * j; // Calculate the next index in the downward direction
+            // Check if i is within the valid range and not visited before
             if (i >= 0 && i < len && !vis[i]) {
-                ans += s[i];
-                vis[i] = 1;
+                ans += s[i]; // Add the character at index i to the answer string
+                vis[i] = 1; // Mark the character as visited
             }
-            i += 2 * j;
+            i += 2 * j; // Calculate the next index in the upward direction
             if (i >= len) {
-                j++;
-                i = j;
+                j++; // Move to the next row
+                i = j; // Reset i to the starting index of the new row
             }
         }
 
