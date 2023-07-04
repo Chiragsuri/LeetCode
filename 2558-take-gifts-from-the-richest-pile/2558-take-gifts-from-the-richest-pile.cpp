c2f99d358963fb8986d9eda8b76ebc7aa7134133
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-04 21:21:37 +0530

    Time: 4 ms (94.82%), Space: 9.2 MB (79.87%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int>pq(gifts.begin(),gifts.end());
+        
+        while(k--){
+            long long temp = pq.top();
+            pq.pop();
+            pq.push(sqrt(temp));
+        }
+        long long total = 0;
+        while (!pq.empty()) {
+            total += pq.top();
+            pq.pop();
+        }
+        return total;
+    }
+};
