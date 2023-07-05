52743129221201e14fdebef789b9a0570a780474
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-05 15:58:56 +0530

    Time: 18 ms (42.89%), Space: 10.2 MB (56.97%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int, int>> q;
+        for (int i = 0; i < score.size(); i++)
+            q.push({score[i], i});
+        
+        int i = 0;
+        vector<string> a(q.size());
+        
+        while (!q.empty()) {
+            a[q.top().second] = (i == 0 ? "Gold Medal" : (i == 1 ? "Silver Medal" : (i == 2 ? "Bronze Medal" : to_string(i + 1))));
+            i++;
+            q.pop();
+        }
+        
+        return a;
+    }
+};
