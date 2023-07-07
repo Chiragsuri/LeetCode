cfaf3693458df5aeb94098c6e3cdc5382bc53ce2
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-07 14:56:47 +0530

    Time: 97 ms (38.01%), Space: 37.3 MB (12.10%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,22 +2,43 @@ typedef pair<int,int> pa;
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pa>diffpq;
+        priority_queue<pa,vector<pa>, greater<pa>>diffpq;
         
         for(auto it:arr){
             diffpq.push({abs(it-x),it});
         }
-        
-        while(diffpq.size()>k)
-            diffpq.pop();
-        
         vector<int>ans;
-        while(!diffpq.empty()){
+        while(k--){
         ans.push_back(diffpq.top().second);
             diffpq.pop();
         }
         sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
+
+
+
+//// already done by max heap
+
+// typedef pair<int,int> pa;
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         priority_queue<pa>diffpq;
+        
+//         for(auto it:arr){
+//             diffpq.push({abs(it-x),it});
+//             if(diffpq.size()>k)
+//             diffpq.pop();
+//         }
+//         vector<int>ans;
+//         while(!diffpq.empty()){
+//         ans.push_back(diffpq.top().second);
+//             diffpq.pop();
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans;
+        
+//     }
+// };
