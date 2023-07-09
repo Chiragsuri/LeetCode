135d8c6b9c01f94c2ee56f0a98b293ca17e8f021
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-09 20:51:40 +0530

    Time: 15 ms (29.41%), Space: 9.3 MB (11.41%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,27 +1,59 @@
-#include <stack>
-#include <string>
-
 class Solution {
 public:
-    int minLength(std::string s) {
-        std::stack<char> st;
+    int minLength(string s) {
+        stack<char> st1, st2;
         
         for (char c : s) {
-            if (!st.empty() && isPair(st.top(), c))
-                st.pop();
-            else
-                st.push(c);
+            st1.push(c);
         }
         
-        return st.size();
+        while (!st1.empty()) {
+            if (!st2.empty() && isPair(st2.top(), st1.top())) {
+                st2.pop();
+            } else {
+                st2.push(st1.top());
+            }
+            
+            st1.pop();
+        }
+        
+        return st2.size();
     }
     
 private:
     bool isPair(char a, char b) {
-        if ((a == 'A' && b == 'B') ||
-            (a == 'C' && b == 'D'))
+        if ((a == 'B' && b == 'A') ||(a == 'D' && b == 'C'))
             return true;
         
         return false;
     }
 };
+
+
+
+//single stack
+
+// class Solution {
+// public:
+//     int minLength(string s) {
+//         stack<char> st;
+        
+//         for (char c : s) {
+//             if (!st.empty() && isPair(st.top(), c))
+//                 st.pop();
+//             else
+//                 st.push(c);
+//         }
+        
+//         return st.size();
+//     }
+    
+// private:
+//     bool isPair(char a, char b) {
+//         if ((a == 'A' && b == 'B') ||
+//             (a == 'C' && b == 'D'))
+//             return true;
+        
+//         return false;
+//     }
+// };
