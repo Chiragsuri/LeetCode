d3bf8cff916405db3b4f5760c867452a276a32e2
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-10 19:11:34 +0530

    Time: 3 ms (95.01%), Space: 8.9 MB (32.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        unordered_map<int,int>umap;
+        stack<int>st;
+        int n = nums2.size();
         
-		// vector to be returned
-        vector<int> rs(n1);
-        
-		// vector for storing next greater element of nums2
-        vector<int> nge(n2);
-        stack<int> st;
-
-        st.push(nums2[n2-1]);
-        nge[n2-1] = -1;
-
-		// getting the next greater element
-        for(int i=n2-2 ; i>=0 ; i--){
-
-            while(st.size() > 0 && nums2[i] >= st.top()){
-                 st.pop();
+        for(int i=n-1; i>=0; i--){
+            int ele = nums2[i];
+            while(!st.empty() && st.top() <= ele){
+                st.pop();
             }
-
-            if(st.size() == 0){
-                 nge[i] = -1;
-            }else{
-                 nge[i] = st.top();
-            }
-
-            st.push(nums2[i]);
-        }   
-        
-		// putting nge in rs(vector to be returned)
-        for(int i=0 ; i<n1 ; i++){
-            auto itr = find(nums2.begin() , nums2.end() , nums1[i]);
-            int idx = itr - nums2.begin();
-            rs[i] = nge[idx];
+            int res = (st.empty()) ? -1 : st.top();
+            umap.insert({ele, res});
+            st.push(ele);
         }
-        return rs;
         
+        vector<int> ans;
+        for(auto x: nums1){
+            ans.push_back(umap[x]);
+        }
+        
+        return ans;
     }
 };
