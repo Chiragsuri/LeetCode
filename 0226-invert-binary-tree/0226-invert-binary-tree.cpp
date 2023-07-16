21a580dcc211c5799a27edd40ba701dae74cc35e
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-16 14:23:47 +0530

    Time: 3 ms (56.58%), Space: 9.6 MB (93.49%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -9,14 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
+// class Solution {
+// public:
     
+//     TreeNode* invertTree(TreeNode* root) {
+//         if(root==NULL) return NULL;
+//         TreeNode* invert = new TreeNode(root->val);
+//         invert->left = invertTree(root->right);
+//         invert->right = invertTree(root->left);
+//         return invert;
+//     }
+// };
+
+
+//IF we dont need any new tree i.e altering the og tree only
+class Solution {
+  public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root==NULL) return NULL;
-        TreeNode* invert = new TreeNode(root->val);
-        invert->left = invertTree(root->right);
-        invert->right = invertTree(root->left);
-        return invert;
-    }
+       if (root) {
+           TreeNode* temp = root->left;
+           root->left = root->right;
+           root->right = temp;
+           invertTree(root->left);
+           invertTree(root->right);
+       }
+       return root;
+   }
 };
