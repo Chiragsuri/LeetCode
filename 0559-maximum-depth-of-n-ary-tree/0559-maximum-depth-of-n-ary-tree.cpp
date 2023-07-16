440551c22d8455f31baeb6d0fd4e32d012bbc179
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-16 15:14:41 +0530

    Time: 12 ms (88.67%), Space: 11 MB (7.09%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.cpp
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.cpp
@@ -18,22 +18,44 @@ public:
 };
 */
 
+// class Solution {
+// public:
+    
+//     void solve(Node* root, int &ans, int temp){
+//         temp++;
+//         ans=max(ans,temp);
+//         for(auto x: root->children){
+//             solve(x,ans,temp);
+//         }    
+//     }
+    
+//     int maxDepth(Node* root) {
+//         int ans=0;
+//         if(!root)
+//             return 0;
+//         solve(root,ans,0);
+//         return ans;
+//     }
+// };
+
+
+
+//iterative
+
 class Solution {
 public:
-    
-    void solve(Node* root, int &ans, int temp){
-        temp++;
-        ans=max(ans,temp);
-        for(auto x: root->children){
-            solve(x,ans,temp);
-        }    
-    }
-    
     int maxDepth(Node* root) {
-        int ans=0;
-        if(!root)
-            return 0;
-        solve(root,ans,0);
-        return ans;
+        if (root == nullptr) return 0;
+        queue<Node*> q; q.push(root);
+        int depth = 0;
+        while (!q.empty()) {
+            depth += 1;
+            int breadth = q.size();
+            for (int _ = 0; _ < breadth; ++_) {
+                auto node = q.front(); q.pop();
+                for (auto child : node->children) if (child) q.push(child);
+            }
+        }
+        return depth;
     }
 };
