859aa1d52a1e6a0457e95337a563a3eba7d76498
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-19 16:56:35 +0530

    Time: 294 ms (53.77%), Space: 146.4 MB (8.80%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        int depth=0;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+           depth++; // Increase the depth for each level of the tree
+
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+
+                // Check if the current node is a leaf node
+                if (!curr->left && !curr->right)
+                    return depth;
+
+                // Add left and right children to the queue if they exist
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+            }
+        }
+        return depth;
+        
+    }
+};
