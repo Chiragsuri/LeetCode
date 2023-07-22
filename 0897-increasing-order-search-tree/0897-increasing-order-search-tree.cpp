cbe8f1ad3e8b0fa8f69dca2e56336419a9f1afe9
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-22 18:32:33 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (36.67%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
@@ -11,18 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* increasingBST(TreeNode* root) {
-        TreeNode* dummy = new TreeNode(0);
-        TreeNode* curr = dummy;
-        inorder(root, curr);
-        return dummy->right;
+    
+    
+    void solve(TreeNode* root, vector<int>&v){
+        if(!root) return;
+        solve(root->left,v);
+        v.push_back(root->val);
+        solve(root->right,v);
     }
     
-    void inorder(TreeNode* node, TreeNode*& curr) {
-        if (!node) return;
-        inorder(node->left, curr);
-        curr->right = new TreeNode(node->val);
-        curr = curr->right;
-        inorder(node->right, curr);
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<int>v;
+        solve(root,v);
+        TreeNode* temp = new TreeNode(v[0]);
+        TreeNode* dummy = temp;
+        for(int i=1;i<v.size();i++){
+            dummy->right=new TreeNode(v[i]);
+            dummy=dummy->right;
+        }
+        return temp;
     }
 };
