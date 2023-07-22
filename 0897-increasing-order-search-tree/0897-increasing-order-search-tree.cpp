da0f481b35bea33862e23d84b065d422a4db5ead
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-22 18:33:14 +0530

    Time: 6 ms (27.69%), Space: 7.9 MB (36.67%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
@@ -11,24 +11,42 @@
  */
 class Solution {
 public:
-    
-    
-    void solve(TreeNode* root, vector<int>&v){
-        if(!root) return;
-        solve(root->left,v);
-        v.push_back(root->val);
-        solve(root->right,v);
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* dummy = new TreeNode(0);
+        TreeNode* curr = dummy;
+        inorder(root, curr);
+        return dummy->right;
     }
     
-    TreeNode* increasingBST(TreeNode* root) {
-        vector<int>v;
-        solve(root,v);
-        TreeNode* temp = new TreeNode(v[0]);
-        TreeNode* dummy = temp;
-        for(int i=1;i<v.size();i++){
-            dummy->right=new TreeNode(v[i]);
-            dummy=dummy->right;
-        }
-        return temp;
+    void inorder(TreeNode* node, TreeNode*& curr) {
+        if (!node) return;
+        inorder(node->left, curr);
+        curr->right = new TreeNode(node->val);
+        curr = curr->right;
+        inorder(node->right, curr);
     }
 };
+
+// class Solution {
+// public:
+    
+    
+//     void solve(TreeNode* root, vector<int>&v){
+//         if(!root) return;
+//         solve(root->left,v);
+//         v.push_back(root->val);
+//         solve(root->right,v);
+//     }
+    
+//     TreeNode* increasingBST(TreeNode* root) {
+//         vector<int>v;
+//         solve(root,v);
+//         TreeNode* temp = new TreeNode(v[0]);
+//         TreeNode* dummy = temp;
+//         for(int i=1;i<v.size();i++){
+//             dummy->right=new TreeNode(v[i]);
+//             dummy=dummy->right;
+//         }
+//         return temp;
+//     }
+// };
