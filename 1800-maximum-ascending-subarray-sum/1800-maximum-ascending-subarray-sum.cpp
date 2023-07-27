1cfde04a58f106487731a814447e92b4a238f985
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-27 19:57:36 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (98.41%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cpp
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = nums[i];
-            for (int j = i; j < nums.size() - 1; j++) {
-                if (nums[j] < nums[j + 1])
-                    sum += nums[j + 1];
-                else
-                    break;
+        int max_sum = nums[0], curr = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i-1] < nums[i]) {
+                curr += nums[i];
             }
-            maxSum = max(maxSum, sum);
+            else {
+                max_sum = max(max_sum, curr);
+                curr = nums[i];
+            }
         }
-        return maxSum;
+        return max(max_sum, curr);
     }
 };
+// class Solution {
+// public:
+//     int maxAscendingSum(vector<int>& nums) {
+//         int maxSum = INT_MIN;
+//         for (int i = 0; i < nums.size(); i++) {
+//             int sum = nums[i];
+//             for (int j = i; j < nums.size() - 1; j++) {
+//                 if (nums[j] < nums[j + 1])
+//                     sum += nums[j + 1];
+//                 else
+//                     break;
+//             }
+//             maxSum = max(maxSum, sum);
+//         }
+//         return maxSum;
+//     }
+// };
