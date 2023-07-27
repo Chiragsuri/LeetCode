bd8ce6b515913e881e530d0d4bc452dd6cfa6f14
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-27 20:20:06 +0530

    Time: 44 ms (99.37%), Space: 33.5 MB (93.63%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,15 +1,36 @@
+//Since, each element in the array is in range [1,n], it means we can use array elements as index for the same array
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = size(nums);
-        vector<int>a(n,0);
-        for(auto it:nums)
-            a[it-1]++;
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(a[i]==0)
-                ans.push_back(i+1);
+        vector<int> res;
+        
+        for( int i=0; i < nums.size(); i++)
+        {
+            int n = abs(nums[i]); 
+            if(nums[n-1] > 0)
+                nums[n-1] = -nums[n-1]; // marking -ve
         }
-        return ans;
+        for( int i=0; i < nums.size(); i++)
+        {
+            if(nums[i]>0)
+                res.push_back(i+1);
+        }
+        return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         int n = size(nums);
+//         vector<int>a(n,0);
+//         for(auto it:nums)
+//             a[it-1]++;
+//         vector<int>ans;
+//         for(int i=0;i<n;i++){
+//             if(a[i]==0)
+//                 ans.push_back(i+1);
+//         }
+//         return ans;
+//     }
+// };
