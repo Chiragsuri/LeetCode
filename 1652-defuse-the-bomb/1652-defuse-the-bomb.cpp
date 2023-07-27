8228c8ea4cc1b09bc233f9ec3e49d5e576e00624
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-27 19:27:41 +0530

    Time: 5 ms (25.77%), Space: 8.2 MB (82.52%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -8,12 +8,12 @@ public:
             int temp = 0;
             
             if (k > 0) {
-                for (int j = 1; j <= k; j++) {
-                    temp += code[(i + j) % n];
+                for (int j = i + 1; j <= i + k; j++) {
+                    temp += code[j % n];
                 }
             } else if (k < 0) {
-                for (int j = n - 1; j >= n + k; j--) {
-                    temp += code[(i + j) % n];
+                for (int j = i - 1; j >= i + k; j--) {
+                    temp += code[(j + n) % n];
                 }
             }
             
