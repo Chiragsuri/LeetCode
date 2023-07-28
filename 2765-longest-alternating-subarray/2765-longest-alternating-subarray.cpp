6b738b905476729068ad0496c12082ad753f96ef
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-28 16:54:51 +0530

    Time: 16 ms (98.92%), Space: 65.1 MB (47.94%) - LeetHub

--- 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
@@ -1,31 +1,65 @@
 class Solution {
 public:
     int alternatingSubarray(vector<int>& nums) {
-    int n = nums.size();
-    int maxi = -1;
-
-    for(int i = 0; i < n-1; i++) {
+        int n = nums.size(),i = 0,j = 1,maxi = -1;
         bool ch = false;
-
-        for(int j = i + 1; j < n; j++) {
-            if(!ch) {
-                if(nums[j] - nums[j-1] == 1) {
-                ch = true;
-                maxi = max(maxi, j - i + 1);
-                } else {
-                break;
+        while(j < n){
+            if(!ch){
+                if(nums[j] - nums[j-1] == 1){
+                    ch = true;
+                    maxi = max(maxi,j - i + 1);
                 }
-            } else {
-                if(nums[j] - nums[j-1] == -1) {
-                ch = false;
-                maxi = max(maxi, j - i + 1);
-                } else {
-                break;
+                else
+                    i = j;
+            }
+            else{
+                if(nums[j] - nums[j-1] == -1){
+                    ch = false;
+                    maxi = max(maxi,j - i + 1);
+                }
+                else if(nums[j] - nums[j-1] == 1){
+                    i = j - 1;
+                    maxi = max(maxi,j - i + 1);
+                }
+                else{
+                    ch = false;
+                    i = j;
                 }
             }
+            j++;
         }
-    }
-
-    return maxi;
+        return maxi;
     }
 };
+
+// class Solution {
+// public:
+//     int alternatingSubarray(vector<int>& nums) {
+//     int n = nums.size();
+//     int maxi = -1;
+
+//     for(int i = 0; i < n-1; i++) {
+//         bool ch = false;
+
+//         for(int j = i + 1; j < n; j++) {
+//             if(!ch) {
+//                 if(nums[j] - nums[j-1] == 1) {
+//                 ch = true;
+//                 maxi = max(maxi, j - i + 1);
+//                 } else {
+//                 break;
+//                 }
+//             } else {
+//                 if(nums[j] - nums[j-1] == -1) {
+//                 ch = false;
+//                 maxi = max(maxi, j - i + 1);
+//                 } else {
+//                 break;
+//                 }
+//             }
+//         }
+//     }
+
+//     return maxi;
+//     }
+// };
