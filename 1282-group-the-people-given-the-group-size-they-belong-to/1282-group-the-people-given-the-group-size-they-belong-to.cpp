b10b4c70b1ce02887d35235356f15c59a6e41d70
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-07-29 15:33:49 +0530

    Time: 8 ms (94.14%), Space: 12.4 MB (99.02%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int, vector<int>> map;
-        vector<vector<int>> res;
-        
-        for (int i = 0; i < groupSizes.size(); i++) {
-            int person = i;
-            int groupSize = groupSizes[i];
-            vector<int>& group = map[groupSize];
-            group.push_back(person);
-            
-            if (group.size() == groupSize) {
-                res.push_back(group);
-                map.erase(groupSize);
+        vector<vector<int>> ans;
+        for (int i = 1; i <= *max_element(groupSizes.begin(), groupSizes.end()); i++) {
+            vector<int> subans;
+            for (int j = 0; j < groupSizes.size(); j++) {
+                if (groupSizes[j] == i) {
+                    if (subans.size() >= i) {
+                        ans.push_back(subans);
+                        subans.clear();
+                    }
+                    subans.push_back(j);
+                }
+            }
+            if (subans.size()) {
+                ans.push_back(subans);
             }
         }
-        
-        return res;
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+//         unordered_map<int, vector<int>> map;
+//         vector<vector<int>> res;
+        
+//         for (int i = 0; i < groupSizes.size(); i++) {
+//             int person = i;
+//             int groupSize = groupSizes[i];
+//             vector<int>& group = map[groupSize];
+//             group.push_back(person);
+            
+//             if (group.size() == groupSize) {
+//                 res.push_back(group);
+//                 map.erase(groupSize);
+//             }
+//         }
+        
+//         return res;
+//     }
+// };
