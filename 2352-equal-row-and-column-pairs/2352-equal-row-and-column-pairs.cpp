b15eb5762370cbcf92a9e8064f618db234a31ac0
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-01 16:52:52 +0530

    Time: 62 ms (99.91%), Space: 32.7 MB (41.81%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-    
-    void solve(vector<vector<int>>& grid, vector<int>& col, int& count, int& n){
-        for(int i=0;i<n;i++){
-            if(col==grid[i]){
-                count++;
-            }
+    int equalPairs(vector<vector<int>>& grid) 
+    {
+        int ans = 0;
+        map<vector<int>, int> mp;
+        
+        for (int i = 0; i < grid.size(); i++)
+            mp[grid[i]]++;
+        
+        for (int i = 0; i < grid[0].size(); i++)
+        {
+            vector<int> v;
+            
+            for (int j = 0; j < grid.size(); j++)
+                v.push_back(grid[j][i]);
+            
+            ans += mp[v];
         }
-    }
-    
-    int equalPairs(vector<vector<int>>& grid) {
-        int count=0;
-        int n=grid.size();
-        int m = grid[0].size();
-        int c=0;
-        while(c<m){
-            vector<int>col;
-            for(int i=0;i<m;i++){
-                col.push_back(grid[i][c]);
-            }
-            solve(grid,col,count,n);
-            c++;
-        }
-        return count;
+        
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+    
+//     int equalPairs(vector<vector<int>>& grid) {
+//         int count=0;
+//         int n=grid.size();
+//         int m = grid[0].size();
+//         int c=0;
+//         while(c<m){
+//             vector<int>col;
+//             for(int i=0;i<m;i++){
+//                 col.push_back(grid[i][c]);
+//             }
+//             for(int i=0;i<n;i++){
+//                 if(col==grid[i]){
+//                     count++;
+//                 }
+//             }
+//             c++;
+//         }
+//         return count;
+//     }
+// };
