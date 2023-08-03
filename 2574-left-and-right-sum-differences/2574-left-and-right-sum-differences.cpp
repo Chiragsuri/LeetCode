cd980cc2695161bcbba1b5255a29731adb42437d
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-03 16:11:13 +0530

    Time: 3 ms (97.74%), Space: 11.3 MB (30.58%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
-        int n=nums.size();
-        
-        vector<int>leftSum(n,0), rightSum(n,0);
-       
-        for(int i=1;i<n;i++)
-            leftSum[i]+=nums[i-1]+leftSum[i-1];
-        
-        for(int i=n-2;i>=0;i--)
-            rightSum[i]+=nums[i+1]+rightSum[i+1];
-        
-        vector<int>ans;
-        
-        for(int i=0;i<n;i++)
-            ans.push_back(abs(leftSum[i]-rightSum[i]));
-        
+        vector<int> ans;
+        int lsum = 0, rsum = 0;
+        for(int i = 0; i < nums.size(); ++i ) rsum += nums[i];
+        for(int i = 0; i < nums.size(); ++i){
+            rsum -= nums[i];
+            ans.push_back(abs(lsum - rsum));
+            lsum += nums[i];
+        }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> leftRightDifference(vector<int>& nums) {
+//         int n=nums.size();
+        
+//         vector<int>leftSum(n,0), rightSum(n,0);
+       
+//         for(int i=1;i<n;i++)
+//             leftSum[i]+=nums[i-1]+leftSum[i-1];
+        
+//         for(int i=n-2;i>=0;i--)
+//             rightSum[i]+=nums[i+1]+rightSum[i+1];
+        
+//         vector<int>ans;
+        
+//         for(int i=0;i<n;i++)
+//             ans.push_back(abs(leftSum[i]-rightSum[i]));
+        
+//         return ans;
+//     }
+// };
