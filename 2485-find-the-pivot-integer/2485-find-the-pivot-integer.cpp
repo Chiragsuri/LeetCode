2082ade63e924f946f78963d4ca567b7f87dace1
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-03 17:29:02 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (20.31%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,14 +1,30 @@
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int l = 1, r = n, sum = n * (n + 1) / 2;
+//         while (l < r) {
+//             int m = (l + r) / 2;
+//             if (m * m - sum < 0)
+//                 l = m + 1;
+//             else
+//                 r = m;
+//         }
+//         return l * l - sum == 0 ? l : -1;
+//     }
+// };
+
 class Solution {
-public:
-    int pivotInteger(int n) {
-        int l = 1, r = n, sum = n * (n + 1) / 2;
-        while (l < r) {
-            int m = (l + r) / 2;
-            if (m * m - sum < 0)
-                l = m + 1;
-            else
-                r = m;
+    public:
+    int pivotInteger(int n) { 
+        int totsum=n*(n+1)/2;
+        
+        int presum=0;
+        
+        for(int i=1; i<=n; i++){
+            presum += i; 
+            if((totsum - presum + i) == presum)
+                return i;
         }
-        return l * l - sum == 0 ? l : -1;
+        return -1;
     }
 };
