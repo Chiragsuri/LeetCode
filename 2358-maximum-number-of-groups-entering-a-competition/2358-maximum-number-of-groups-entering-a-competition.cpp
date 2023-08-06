476debd692172d19a317d761f1eb63d32a02378b
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-06 20:28:47 +0530

    Time: 70 ms (92.95%), Space: 56.6 MB (6.28%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -1,6 +1,20 @@
 class Solution {
-public:
+public: 
     int maximumGroups(vector<int>& grades) {
-        return (int)((sqrt(grades.size() * 8 + 1) - 1)/2);
+        int l = 1, r = 446, n = grades.size();
+        while (l < r) {
+            int m = (l + r + 1) / 2;
+            if (m * (m + 1) / 2 <= n)
+                l = m;
+            else
+                r = m - 1;
+        }
+        return l;
     }
 };
+// class Solution {
+// public:
+//     int maximumGroups(vector<int>& grades) {
+//         return (int)((sqrt(grades.size() * 8 + 1) - 1)/2);
+//     }
+// };
