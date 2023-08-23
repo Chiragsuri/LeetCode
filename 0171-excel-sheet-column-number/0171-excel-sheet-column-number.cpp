d514ea26bffe226419e8f71b65edd3e47d79b769
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-23 16:32:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (33.75%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,3 +1,8 @@
+/* 1. There are 26 letters in our alphabet and we start counting from 1, not zero.
+    So 'Z' is 26.
+ 2. The rest of the combinations start from a base 26 */
+
+
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
