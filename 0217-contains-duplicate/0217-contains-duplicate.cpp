0be0564bdc1c2d2d3e7c4b96361c3076bc5ca178
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-25 20:51:22 +0530

    Time: 122 ms (62.97%), Space: 71.8 MB (11.90%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,23 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=1;i<nums.size();i++)
+//             if(nums[i]==nums[i-1]) return true;
+        
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++)
-            if(nums[i]==nums[i-1]) return true;
-        
-        return false;
-    }
+  bool containsDuplicate(vector<int>& nums) {
+      unordered_map<int,int> mp;
+      for(auto i : nums) mp[i]++;
+      bool flag = false;
+      for(auto i : mp){
+          if(i.second >= 2) return true;
+      }
+      return flag;
+  }
 };
