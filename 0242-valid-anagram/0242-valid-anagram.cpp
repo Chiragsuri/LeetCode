74e405843b2d07ee5d04f187831117cbdf62cafb
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-26 18:10:15 +0530

    Time: 11 ms (54.55%), Space: 7.5 MB (17.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),end(s));
-        sort(begin(t),end(t));
-        return s==t;
+        unordered_map<int,int>m1,m2;
+        for(auto it:s)
+            m1[it]++;
+        for(auto it:t)
+            m2[it]++;
+        return m1==m2;
     }
 };
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(),end(s));
+//         sort(begin(t),end(t));
+//         return s==t;
+//     }
+// };
