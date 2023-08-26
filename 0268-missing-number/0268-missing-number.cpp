e386b456e1056717265e9b77358396dd5047d85e
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-26 18:23:25 +0530

    Time: 30 ms (13.41%), Space: 24.2 MB (5.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        for(int i=0;i<=nums.size();i++){
-            if(find(nums.begin(),nums.end(),i)==nums.end())
+        unordered_map<int,int>mp;
+        for(auto it:nums)
+            mp[it]++;
+        for(int i=0;i<=nums.size();i++)
+            if(!mp[i])
                 return i;
-        }
         return 0;
     }
 };
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         for(int i=0;i<=nums.size();i++){
+//             if(find(nums.begin(),nums.end(),i)==nums.end())
+//                 return i;
+//         }
+//         return 0;
+//     }
+// };
