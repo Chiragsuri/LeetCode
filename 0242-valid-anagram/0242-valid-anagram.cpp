c8a7488031690b07b39800f5b1667e7b2641a023
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-26 18:12:27 +0530

    Time: 3 ms (98.08%), Space: 7.3 MB (49.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,34 @@
-class Solution {
+class Solution {               
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<int,int>m1,m2;
-        for(auto it:s)
-            m1[it]++;
-        for(auto it:t)
-            m2[it]++;
-        return m1==m2;
+        if(s.length() != t.length()){
+            return false;
+        }
+        int arr[26] = {0};
+        for(int i=0; i<s.length(); i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         unordered_map<int,int>m1,m2;
+//         for(auto it:s)
+//             m1[it]++;
+//         for(auto it:t)
+//             m2[it]++;
+//         return m1==m2;
+//     }
+// };
 
 // class Solution {
 // public:
