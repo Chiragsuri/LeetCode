6793164db32aaf3582fbfd24299a0e0b92668ed6
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-08-27 22:48:13 +0530

    Time: 31 ms (67.44%), Space: 10.9 MB (20.23%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,21 +1,36 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         unordered_map<char,int> m;
+//        queue<pair<char,int>> q;
+//         for(int i=0;i<s.length();i++){
+//             m[s[i]]++;
+//             q.push({s[i],i});
+//         }
+//         while(!q.empty()){
+//             if(m[q.front().first]==1) return q.front().second;
+//             else{
+//                 q.pop();
+//             }
+//         }
+//         return -1;
+       
+//     }
+    
+// };
+
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> m;
-       queue<pair<char,int>> q;
-        for(int i=0;i<s.length();i++){
-            m[s[i]]++;
-            q.push({s[i],i});
-        }
-        while(!q.empty()){
-            if(m[q.front().first]==1) return q.front().second;
-            else{
-                q.pop();
+        unordered_map<char, int> mp;       
+        for (auto ch : s) {
+            mp[ch]++;
+        }        
+        for (int i = 0; i < s.size(); i++) {
+            if (mp[s[i]] == 1) {
+                return i;
             }
         }
         return -1;
-       
     }
-    
 };
-
