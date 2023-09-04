ee433d51a165331b8e58e93ad756594c62332522
Chirag Suri <121798705+Chiragsuri@users.noreply.github.com>
2023-09-04 17:05:47 +0530

    Time: 86 ms (61.92%), Space: 67.8 MB (9.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,49 @@
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         ///kadane algo//
+//         int sum=0;
+//         int maxi=nums[0];
+//         for(int num:nums){
+//             sum=sum+num;
+//             maxi=max(maxi,sum);
+//             if(sum<0) sum=0;
+//         }
+//         return maxi;
+//     }
+// };
+
+//Printing SUbarray
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        ///kadane algo//
-        int sum=0;
-        int maxi=nums[0];
-        for(int num:nums){
-            sum=sum+num;
-            maxi=max(maxi,sum);
-            if(sum<0) sum=0;
+        int n = nums.size();
+        
+        int maxi = nums[0];
+        int start = 0, end = 0; // the final start and end position of the maximum sum subarray
+        
+        int sum = 0;
+        int s = 0; // the temporary start position
+        
+        for(int i=0;i<n;i++){
+            
+            sum += nums[i];
+      
+            
+            if(sum > maxi){
+                maxi = sum;
+                start = s;
+                end = i;
+            }
+            
+            if(sum < 0){
+                sum = 0;
+                s = i+1;
+            }   
         }
-        return maxi;
+        
+        cout<<"Maximum Sum Subarray from nums["<<start<<"] = "<<nums[start]<<" till nums["<<end<<"] = "<<nums[end]<<endl;
+        
+        return maxi; 
     }
 };
